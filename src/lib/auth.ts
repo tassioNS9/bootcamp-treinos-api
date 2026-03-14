60910ad59f9a0ca5d3671da42cd2ef8ee86d38df
Tassio Neves <tassions@hotmail.com>
2026-03-14 16:06:45 -0300

    fix: update BETTER_AUTH_URL for Google sign-in

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -6,13 +6,15 @@ import { prisma } from "./db.js";
 
 export const auth = betterAuth({
   // Permitir que o frontend acesse os cookies de autenticação
-  baseURL: process.env.BETTER_AUTH_SECRET as string,
+  baseURL: process.env.BETTER_AUTH_URL as string,
   trustedOrigins: ["http://localhost:3000"],
   emailAndPassword: {
     enabled: true,
   },
   socialProviders: {
     google: {
+      // O prompt select acount
+      //prompt: "select_account",
       clientId: process.env.GOOGLE_CLIENT_ID as string,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
     },
