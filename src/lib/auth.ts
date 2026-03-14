42428df1cd5fdceef00c6f00ffbcc7e2f566e1d1
Tassio Neves <tassions@hotmail.com>
2026-03-14 15:07:45 -0300

    feat: add sign in with google

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -6,9 +6,16 @@ import { prisma } from "./db.js";
 
 export const auth = betterAuth({
   // Permitir que o frontend acesse os cookies de autenticação
+  baseURL: process.env.BETTER_AUTH_SECRET as string,
   trustedOrigins: ["http://localhost:3000"],
   emailAndPassword: {
     enabled: true,
+  },
+  socialProviders: {
+    google: {
+      clientId: process.env.GOOGLE_CLIENT_ID as string,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+    },
   },
   database: prismaAdapter(prisma, {
     provider: "postgresql",
